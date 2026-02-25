ea39c2fedd33dd72b1b24119494a038533276a27
Nafiz Alam <nafizalam.dev@gmail.com>
2026-02-26 02:20:20 +0600

    JS Practice

---
+++ JS/script.js
@@ -0,0 +1,7 @@
+function changeStyle() {
+    let box = document.getElementById("box");
+    box.style.backgroundColor = "tomato";
+    box.style.color = "white";
+    box.style.borderRadius = "24px";
+    box.style.marginBottom = "20px";
+}
