1017ed6e4efe66118f596cca6384c905be2f6718
Nafiz Alam <nafizalam.dev@gmail.com>
2026-02-27 00:46:47 +0600

    JS Practice

---
+++ JS/event.js
@@ -0,0 +1,17 @@
+function changeBgColor() {
+    const btn = document.querySelector('button');
+    btn.style.backgroundColor = 'lightblue';
+}
+
+function makeBgRed() {
+    document.body.style.backgroundColor = 'red';
+}
+
+function makeBgGreen() {
+    document.body.style.backgroundColor = 'green';
+}
+
+function makeBgWhite() {
+    const btn = document.querySelector('button');
+    btn.style.backgroundColor = 'white';
+}
