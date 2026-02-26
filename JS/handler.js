1ad18cc274cd0f99d8f8480545232b51e9e5e609
Nafiz Alam <nafizalam.dev@gmail.com>
2026-02-27 02:35:42 +0600

    JS Practice

--- JS/handler.js
+++ JS/handler.js
@@ -2,10 +2,19 @@
     const pageTitle = document.getElementById('page_title');
     pageTitle.innerText = 'Updated page title text';
 });
-*/
+
 
 document.getElementById('login').addEventListener('click', function() {
     const user = document.getElementById('user');
 
     user.innerText = 'User Login';
+});
+
+*/
+
+document.getElementById('updateButton').addEventListener('click', function() {
+    const inputText = document.getElementById('input_text');
+    const name = document.getElementById('name');
+
+    name.innerText = inputText.value;
 });
