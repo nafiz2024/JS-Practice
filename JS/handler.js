8d269134fb29607f9ba99e4bdd4be1e9174411fc
Nafiz Alam <nafizalam.dev@gmail.com>
2026-02-27 02:24:59 +0600

    JS Practice

---
+++ JS/handler.js
@@ -0,0 +1,6 @@
+/* document.getElementById('updateTitleBtn').addEventListener('click', function () {
+    const pageTitle = document.getElementById('page_title');
+    pageTitle.innerText = 'Updated page title text';
+});
+*/
+
